9115ac38aa3ac3dc79a7468ce86d2cdf7b0596b9
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-07-22 21:32:31 -0400

    add: models, enums

---
+++ Monix.Core/Enums/ETransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monix.Core.Enums
+{
+    public enum ETransactionType
+    {
+        Deposit = 0,
+        Withdraw = 1
+    }
+}
---
+++ Monix.Core/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monix.Core.Enums;
+
+namespace Monix.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime PaidOrReceivedAt { get; set; }
+        public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
+        public decimal Amount { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public string UserId { get; set; } = String.Empty;
+    }
+}
