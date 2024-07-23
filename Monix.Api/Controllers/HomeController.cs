c24d8dbfa9c84a1bccf716a4f891dce8efbb8ac7
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-07-22 22:16:17 -0400

    add: basic controllers

---
+++ Monix.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Monix.Api.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
