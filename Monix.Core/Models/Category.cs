b462232c043176672e54525803ea85dc730d1a55
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-07-22 21:21:03 -0400

    Add project files.

---
+++ Monix.Core/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monix.Core.Models
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string UserId { get; set; } = String.Empty;
+    }
+}
