2614e8e4d4d4fd562262f3feae190fcfc75ef9a2
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-07-24 21:27:18 -0400

    add: Transaction mapping

---
+++ Monix.Api/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Monix.Core.Models;
+
+namespace Monix.Api.Data.Mappings
+{
+    public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+            builder.Property(x => x.Type)
+                .IsRequired()
+                .HasColumnType("SMALLINT");
+            builder.Property(x => x.Amount)
+                .IsRequired()
+                .HasColumnType("MONEY");
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+            builder.Property(x => x.PaidOrReceivedAt)
+                .IsRequired(false);
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(160);
+        }
+    }
+}
