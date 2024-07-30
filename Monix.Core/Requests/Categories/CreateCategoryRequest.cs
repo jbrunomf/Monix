e68d20ad75e27d9fd641e92899abfef9b3734f81
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-07-29 21:57:12 -0400

    Criando classe para padronizar a entrada de dados/validações

---
+++ Monix.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monix.Core.Requests.Categories
+{
+    public class CreateCategoryRequest : Request
+    {
+        [Required(ErrorMessage = "Título inválido.")]
+        [MaxLength(80, ErrorMessage = "{0} Deve conter até {1} caracteres.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Descrição inválida.")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
